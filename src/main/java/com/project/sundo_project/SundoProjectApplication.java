b78257075858ec4ae85a12b203bd61304d9df38e
junwon-hwnag <nyong1320@gmail.com>
2024-08-06 10:35:46 +0900

    "first commit"

---
+++ src/main/java/com/project/sundo_project/SundoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.project.sundo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundoProjectApplication.class, args);
+
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/project/sundo_project/SundoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sundo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
