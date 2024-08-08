1d52cd6ddbe232f003c95771ea7eee7b0f1aad9e
junwon-hwang <nyong1320@gmail.com>
2024-08-08 15:25:45 +0900

    "평가 DB저장 로직 구현"

--- src/main/java/com/project/sundo_project/entity/Evaluation.java
+++ src/main/java/com/project/sundo_project/entity/Evaluation.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
+@Getter @Setter
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
--- src/main/java/com/project/sundo_project/service/EvaluationService.java
+++ src/main/java/com/project/sundo_project/service/EvaluationService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -19,8 +21,14 @@ public class EvaluationService {
     public void save(EvaluationSaveDto dto){
 
         Evaluation evaluation = dto.toEntity();
-        log.info("evaluation : {}",evaluation);
 
+        int avg = (evaluation.getWindVolume() + evaluation.getNoiseLevel()
+                + evaluation.getScenery() + evaluation.getWaterDepth()) / 4;
+
+        evaluation.setAverageRating(avg);
+        evaluation.setPriRegistrationDate(LocalDateTime.now());
+
+        log.info("evaluation + avg :{} " , evaluation);
 
         evaluationRepository.save(evaluation);
     }
